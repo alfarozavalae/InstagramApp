ee977b4f3532b014f6cf98d9db938e2b9b2e341f
alfarozavalae <Newgirl1>
2019-11-17 23:44:17 -0500

    Adding files

--- app/src/main/java/com/codepath/emely/instagram/LoginActivity.java
+++ app/src/main/java/com/codepath/emely/instagram/LoginActivity.java
@@ -3,17 +3,27 @@ package com.codepath.emely.instagram;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.os.Bundle;
 import android.widget.Button;
 import android.view.View.OnClickListener;
+import android.widget.TextView;
+
+import com.parse.LogInCallback;
+import com.parse.ParseException;
+import com.parse.ParseUser;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private static final String TAG = "LoginActivity";
 
     private EditText etUsername;
     private EditText etPassword;
     private Button btnLogin;
+    private TextView CreateAccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +33,16 @@ public class LoginActivity extends AppCompatActivity {
         etUsername = findViewById(R.id.etUsername);
         etPassword = findViewById(R.id.etPassword);
         btnLogin = findViewById(R.id.btnLogin);
+        CreateAccount = findViewById(R.id.CreateAccount);
+
+        CreateAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
+                startActivity(intent);
+            }
+        });
+
         btnLogin.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -33,9 +53,24 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
     }
-    
     private void login(String username, String password) {
-        // TODO: navigate to the new activity if the user has signed properly
+        ParseUser.logInInBackground(username, password, new LogInCallback() {
+            @Override
+            public void done(ParseUser user, ParseException e) {
+                //TODO: add better error handling
+                if(e != null) {
+                    Log.e(TAG, "Issue with Login");
+                    e.printStackTrace();
+                    return;
+                }
+                goMainActivity();
+            }
+        });    }
 
+    private void goMainActivity() {
+        Log.d(TAG, "Navigating to Main Activity");
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
--- app/src/main/java/com/codepath/emely/instagram/ParseApplication.java
+++ app/src/main/java/com/codepath/emely/instagram/ParseApplication.java
@@ -3,18 +3,21 @@ package com.codepath.emely.instagram;
 import android.app.Application;
 
 import com.parse.Parse;
+import com.parse.ParseObject;
 
 public class ParseApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
 
+        ParseObject.registerSubclass(Post.class);
     // set applicationId, and server server based on the values in the Heroku settings.
     // clientKey is not needed unless explicitly configured
-    // any network interceptors must be added with the Configuration Builder given this syntax
-    Parse.initialize(new Parse.Configuration.Builder(this)
-            .applicationId("myAppId") // should correspond to APP_ID env variable
-            .clientKey(null)  // set explicitly unless clientKey is explicitly configured on Parse server
-            .server("https://my-parse-app-url.herokuapp.com/parse/").build());
+    // any network interceptors must be added with the Configuration Builder given this syntaxParse.enableLocalDatastore(this);
+        Parse.enableLocalDatastore(this);
+        Parse.initialize(new Parse.Configuration.Builder(this)
+            .applicationId("emely-instagram-codepath") // should correspond to APP_ID env variable
+            .clientKey("CodepathInstagramEmely")  // set explicitly unless clientKey is explicitly configured on Parse server
+            .server("https://emely-instagram-codepath.herokuapp.com/parse/").build());
     }
 }
